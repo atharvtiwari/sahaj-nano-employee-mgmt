c5103c8332ec853d7db2e2a5d4efded82eed05ed
atharvtiwari <atharvtiwari@users.noreply.github.com>
2023-08-24 16:43:52 +0530

    changes

--- server.py
+++ server.py
@@ -15,8 +15,7 @@ def save_data_to_file(data, json_file : str):
     with open(json_file, 'w') as file:
         json.dump(data, file)
 
-employees = []
-deleted_employees = []
+employees = load_data_from_file('employees.json')
 
 # Greeting 
 @app.route("/greeting", methods=['GET'])
@@ -30,7 +29,7 @@ def create_employee():
     employee_id = str(len(employees) + 1)
     employee.update({"employeeId" : employee_id})
     employees.append(employee)
-    save_data_to_file(employees, '/home/employees.json')
+    save_data_to_file(employees, 'employees.json')
     return jsonify({"employeeId" : employee_id}), 201
 
 # Get all Employee details
@@ -41,7 +40,7 @@ def get_all_employees():
 # Get Employee details
 @app.route('/employee/<id>', methods=['GET'])
 def get_employee(id):
-    employee = [i for i in employees if i["employeeID"] == id][0]
+    employee = next((item for item in employees if item["employeeId"] == id), None)
     if (employee):
         return jsonify(employee), 200
     else:
@@ -50,12 +49,13 @@ def get_employee(id):
 # Update Employee
 @app.route('/employee/<id>', methods=['PUT'])
 def update_employee(id):
-    employee = [i for i in employees if i["employeeID"] == id][0]
+    employee = next((item for item in employees if item["employeeId"] == id), None)
     if (employee):
         updated_employee = request.get_json()
+        old_employee= employee
         employee.update(updated_employee)
-        employees[int(id) - 1] = employee
-        save_data_to_file(employees, '/home/employees.json')
+        employees[employees.index(old_employee)] = employee
+        save_data_to_file(employees, 'employees.json')
         return jsonify(employee), 201
     else:
         return jsonify({ "message" : "Employee with " + id + " was not found" }), 404   
@@ -63,16 +63,13 @@ def update_employee(id):
 # Delete Employee
 @app.route('/employee/<id>', methods=['DELETE'])
 def delete_employee(id):
-    employee = [i for i in employees if i["employeeID"] == id][0]
+    employee =next((item for item in employees if item["employeeId"] == id), None)
     if (employee):
-        deleted_employees.append(employee)
         employees.remove(employee)
-        save_data_to_file(employees, '/home/employees.json')
-        save_data_to_file(deleted_employees, '/home/deleted_employees.json')
+        save_data_to_file(employees, 'employees.json')
         return jsonify(employee), 200
     else:
         return jsonify({ "message" : "Employee with " + id + " was not found" }), 404
-
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
