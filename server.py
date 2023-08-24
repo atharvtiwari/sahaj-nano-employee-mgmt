78e5b801d8829c7e72f3ad9dec2bcd346244ab75
atharvtiwari <atharvtiwari@users.noreply.github.com>
2023-08-24 17:10:38 +0530

    changed list to dict for quicker access

--- server.py
+++ server.py
@@ -2,71 +2,63 @@ from flask import Flask, request, jsonify
 import json
 
 app = Flask(__name__)
+app.config['EMPLOYEES_JSON_FILE'] = '/home/employees.json'
 
-def load_data_from_file(json_file : str):
+def load_data_from_file():
     try:
-        with open(json_file, 'r') as file:
-            data = json.load(file)
+        with open(app.config['EMPLOYEES_JSON_FILE'], 'r') as file:
+            return json.load(file)
     except FileNotFoundError:
-        data = []
-    return data
+        return {}
 
-def save_data_to_file(data, json_file : str):
-    with open(json_file, 'w') as file:
+def save_data_to_file(data):
+    with open(app.config['EMPLOYEES_JSON_FILE'], 'w') as file:
         json.dump(data, file)
 
-employees = load_data_from_file('employees.json')
+employees = load_data_from_file()
 
 # Greeting 
 @app.route("/greeting", methods=['GET'])
 def greeting():
     return 'Hello world!', 200
 
-# Create Employee
 @app.route('/employee', methods=['POST'])
 def create_employee():
     employee = request.get_json()
     employee_id = str(len(employees) + 1)
     employee.update({"employeeId" : employee_id})
-    employees.append(employee)
-    save_data_to_file(employees, 'employees.json')
+    employees[employee_id] = employee
+    save_data_to_file(employees)
     return jsonify({"employeeId" : employee_id}), 201
 
-# Get all Employee details
 @app.route('/employees/all', methods=['GET'])
 def get_all_employees():
-    return jsonify(employees), 200
+    return jsonify(list(employees.values())), 200
 
-# Get Employee details
 @app.route('/employee/<id>', methods=['GET'])
 def get_employee(id):
-    employee = next((item for item in employees if item["employeeId"] == id), None)
-    if (employee):
+    employee = employees.get(id)
+    if employee:
         return jsonify(employee), 200
     else:
         return jsonify({ "message" : "Employee with " + id + " was not found" }), 404
 
-# Update Employee
 @app.route('/employee/<id>', methods=['PUT'])
 def update_employee(id):
-    employee = next((item for item in employees if item["employeeId"] == id), None)
-    if (employee):
+    employee = employees.get(id)
+    if employee:
         updated_employee = request.get_json()
-        old_employee= employee
         employee.update(updated_employee)
-        employees[employees.index(old_employee)] = employee
-        save_data_to_file(employees, 'employees.json')
+        save_data_to_file(employees)
         return jsonify(employee), 201
     else:
         return jsonify({ "message" : "Employee with " + id + " was not found" }), 404   
 
-# Delete Employee
 @app.route('/employee/<id>', methods=['DELETE'])
 def delete_employee(id):
-    employee =next((item for item in employees if item["employeeId"] == id), None)
-    if (employee):
-        employees.remove(employee)
-        save_data_to_file(employees, 'employees.json')
+    employee = employees.pop(id, None)
+    if employee:
+        save_data_to_file(employees)
         return jsonify(employee), 200
     else:
         return jsonify({ "message" : "Employee with " + id + " was not found" }), 404
