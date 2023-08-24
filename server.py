12e35ef7cf7910d589bf48a1e3ea5e3757bbf2ac
atharvtiwari <atharvtiwari@users.noreply.github.com>
2023-08-24 16:00:31 +0530

    fix bug + try persistence

--- server.py
+++ server.py
@@ -1,8 +1,22 @@
 from flask import Flask, request, jsonify
+import json
+
 app = Flask(__name__)
 
-employees = []
-deleted_employees = []
+def load_data_from_file(json_file : str):
+    try:
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        data = []
+    return data
+
+def save_data_to_file(data, json_file : str):
+    with open(json_file, 'w') as file:
+        json.dump(data, file)
+
+employees = load_data_from_file('/home/employees.json')
+deleted_employees = load_data_from_file('/home/employees.json')
 
 # Greeting 
 @app.route("/greeting", methods=['GET'])
@@ -16,6 +30,7 @@ def create_employee():
     employee_id = str(len(employees) + 1)
     employee.update({"employeeId" : employee_id})
     employees.append(employee)
+    save_data_to_file(employees, '/home/employees.json')
     return jsonify({"employeeId" : employee_id}), 201
 
 # Get all Employee details
@@ -26,7 +41,7 @@ def get_all_employees():
 # Get Employee details
 @app.route('/employee/<id>', methods=['GET'])
 def get_employee(id):
-    employee = [i for employee in employees if i['employeeID'] == id][0]
+    employee = [i for i in employees if i['employeeID'] == id][0]
     if (employee):
         return jsonify(employee), 200
     else:
@@ -35,11 +50,12 @@ def get_employee(id):
 # Update Employee
 @app.route('/employee/<id>', methods=['PUT'])
 def update_employee(id):
-    employee = [i for employee in employees if i['employeeID'] == id][0]
+    employee = [i for i in employees if i['employeeID'] == id][0]
     if (employee):
         updated_employee = request.get_json()
         employee.update(updated_employee)
         employees[int(id) - 1] = employee
+        save_data_to_file(employees, '/home/employees.json')
         return jsonify(employee), 201
     else:
         return jsonify({ "message" : "Employee with " + id + " was not found" }), 404   
@@ -47,10 +63,12 @@ def update_employee(id):
 # Delete Employee
 @app.route('/employee/<id>', methods=['DELETE'])
 def delete_employee(id):
-    employee = [i for employee in employees if i['employeeID'] == id][0]
+    employee = [i for i in employees if i['employeeID'] == id][0]
     if (employee):
         deleted_employees.append(employee)
         employees.remove(employee)
+        save_data_to_file(employees, '/home/employees.json')
+        save_data_to_file(deleted_employees, '/home/deleted_employees.json')
         return jsonify(employee), 200
     else:
         return jsonify({ "message" : "Employee with " + id + " was not found" }), 404
